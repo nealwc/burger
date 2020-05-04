c8435d5082f03f9c8cc250e6b120c6a1dabd429c
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-05-03 21:09:24 -0400

    model setup

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+const orm = require("../config/orm")
+
+const burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+  };
+  
+  // Export the database functions for the controller (burger_controller.js).
+  module.exports = burger;
