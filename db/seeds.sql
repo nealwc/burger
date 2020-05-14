e60b232041d3d35da27534a203ae4ba6566cc5d1
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-05-13 21:43:03 -0400

    added more seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
-INSERT INTO burgers (burger_name) VALUES ('Single');
-INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ("Single");
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Mushroom and Swiss Cheeseburger")
